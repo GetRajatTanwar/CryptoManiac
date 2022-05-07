a59f953279060af5b227e3a59a05a3ad2914f845
Rajat Tanwar <getrajattanwar@gmail.com>
2022-05-07 22:19:24 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB9B6vgJ5z3f-gd0rTlJUKXqy03JFEsun0",
+    authDomain: "crypto-maniac-bab12.firebaseapp.com",
+    projectId: "crypto-maniac-bab12",
+    storageBucket: "crypto-maniac-bab12.appspot.com",
+    messagingSenderId: "700318412934",
+    appId: "1:700318412934:web:f737dfd3f5dfe710ea2107"
+  };
+
+  export default firebaseConfig;
